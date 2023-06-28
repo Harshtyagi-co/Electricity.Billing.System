0c4662b4cbcab59b5c9b9f7c16ec6883d803db17
Harshtyagi-co <84906229+Harshtyagi-co@users.noreply.github.com>
2023-06-28 10:40:16 +0530

    Added some file

---
+++ src/electricity/splash.java
@@ -0,0 +1,14 @@
+package electricity;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+    Splash(){
+        setSize(400,400);
+        setLocation(300,100);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
