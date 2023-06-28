bf6b495277ec7827d79569e44d753535fa42731c
Harshtyagi-co <84906229+Harshtyagi-co@users.noreply.github.com>
2023-06-28 11:37:49 +0530

    Done some changes

--- src/electricity/splash.java
+++ src/electricity/splash.java
@@ -1,11 +1,17 @@
 package electricity;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame {
     Splash(){
-        setSize(400,400);
-        setLocation(300,100);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon.splash/Splash.jpg"));
+        Image imageOne = imageIcon.getImage().getScaledInstance(600,400,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon2 = new ImageIcon(imageOne);
+        JLabel imageLabel = new JLabel(imageIcon2.,);
+        add(imageLabel);
+        setSize(600,400);
+        setLocation(500,200);
         setVisible(true);
     }
     public static void main(String[] args) {
